9befdcd88d28967e8c3b2324ef09c92d4d95e440
ahmedlabib02 <ahmedlabibamer@gmail.com>
2024-09-28 13:38:36 +0300

    step one is done I think

---
+++ src/Flower.js
@@ -0,0 +1,15 @@
+class Flower{
+    constructor(center,cenRed,cenGreen,cenBlue, petalsRed, petalsGreen, petalsBlue, petalsNum ){
+            this.center=center;
+            this.cenRed = cenRed;
+            this.cenBlue=cenBlue;
+            this.petalsRed=petalsRed
+            this.petalsGreen=petalsGreen;
+            this.petalsBlue=petalsBlue;
+            this.petalsNum = petalsNum;
+    }
+    
+
+
+
+}
